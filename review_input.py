8dce18c599432996d78b55cc12cade5f72faafcc
しろあり <siromk175@gmail.com>
2025-01-25 15:32:21 +0900

    画面作成途中

--- review_input.py
+++ review_input.py
@@ -6,23 +6,28 @@ def scrape():
     input_value = entry.get()
     messagebox.showinfo("情報", f"'{input_value}'のレビューをスクレイピングを開始します。")
     # ここに実際のスクレイピングコードを追加
-    print(f"'{input_value}'のレビューをスクレイピングを開始します。")
+    # print(f"'{input_value}'のレビューをスクレイピングを開始します。")
 
 # Tkinterのウィンドウを作成
 root = tk.Tk()
 root.title("楽天レビュースクレイピング")
+root.geometry("800x450")
+
+# フレーム
+frame = tk.Frame(root, width=750, height=200, bg='#888888', relief="solid")
+frame.pack(padx=10, pady=10)
 
 # ラベルを作成
-label = tk.Label(root, text="レビューをスクレイピングする商品名を入力してください：")
-label.pack()
+label = tk.Label(frame, text="レビューをスクレイピングするURLを入力してください：", font=("游ゴシック", 12))
+label.place(x=10, y=10)
 
 # 入力項目を作成
-entry = tk.Entry(root)
-entry.pack()
+entry = tk.Entry(frame, width=80, font=("游ゴシック", 12) )
+entry.place(x=10, y=50)
 
 # ボタンを作成
-button = tk.Button(root, text="スクレイピング開始", command=scrape)
-button.pack()
+button = tk.Button(frame, text="スクレイピング開始", font=("游ゴシック", 12), command=scrape)
+button.place(x=10, y=110)
 
 # メインループを開始
 root.mainloop()
